e9843e72d05e2483d721f931bdc2094d9765aae9
Sourabh Malviya <sourabhm125@gmail.com>
2026-05-19 12:02:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-blue-500">Welcome to My React App!</h1>
+    </div>
+  );
+}
+
+export default App;
